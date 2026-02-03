c9f3b0b986b5dba4e81d799fa7b1b4f699741bfa
Bright Onyekachi <brightajuzie@gmail.com>
2026-02-03 15:49:02 +0100

    vehiclse

--- routes/static.js
+++ routes/static.js
@@ -1,5 +1,8 @@
+const utilities = require("../utilities/")
 const express = require('express');
 const router = express.Router();
+
+const invCont = require("../controllers/invController") // Ensure this path matches your controller's name
 
 // The Home Route belongs HERE, not in server.js
 router.get("/", (req, res) => {
